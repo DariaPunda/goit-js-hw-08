95df1ae9c23dd4fd0b4d55d6c7f7665a426f9afd
DariaPunda <d.punda19@gmail.com>
2023-03-14 17:33:04 +0200

    3 TASK

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
 
+import throttle from "lodash.throttle";
+const formInput = document.querySelector('.feedback-form ');
+
+const inputInfo = {email: '', message: ''};
+
+formInput.addEventListener('input', throttle(function (evt) {
+  const {name , value} = evt.target
+    inputInfo[name] = value;
+    console.log(inputInfo);
+    
+    localStorage.setItem('feedback-form-state', JSON.stringify(inputInfo));
+
+}, 500));
+
+const getFormInfo = localStorage.getItem('feedback-form-state');
+if (getFormInfo) {
+    const formLocalInfo = JSON.parse(getFormInfo);
+    formInput.email.value = formLocalInfo.email;
+    formInput.message.value = formLocalInfo.message;
+};
+
+formInput.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    console.log(getFormInfo);
+    evt.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+})
+
